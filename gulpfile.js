e12d951ba573b6875047d6d1e385d6b18323cdde
Luan Gjokaj <luan@riangle.com>
2021-09-04 13:47:02 +0200

    chore: update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const { gulp, series, parallel, dest, src, watch } = require("gulp");
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
 const cssnano = require("gulp-cssnano");
 const autoprefixer = require("gulp-autoprefixer");
